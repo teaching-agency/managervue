1e37114a37fc24c74d6ada07aa5ba8186588d01f
WangMingZhi123 <Wmz19960224@163.com>
2019-12-26 10:54:25 +0800

    修改title/修改登录页面/去除生产环境加载console

--- src/components/page/login/loginpage.vue
+++ src/components/page/login/loginpage.vue
@@ -2,9 +2,27 @@
   登录页
 -->
 <template>
-    <div class="aaa">
-      成功进入登录页面！！！！！
-    </div>
+  <div class="login-box">
+    <!-- 通过:rules="loginFormRules"来绑定输入内容的校验规则 -->
+    <el-form :rules="loginFormRules" ref="loginForm" :model="loginForm" label-position="right" label-width="auto" show-message>
+      <span class="login-title">欢迎登录</span>
+      <div style="margin-top: 5px"></div>
+      <el-form-item label="用户名" prop="loginName">
+        <el-col :span="22">
+          <el-input type="text" v-model="loginForm.loginName"></el-input>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="密码" prop="loginPassword">
+        <el-col :span="22">
+          <el-input type="password" v-model="loginForm.loginPassword"></el-input>
+        </el-col>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="loginSubmit('loginForm')">登录</el-button>
+        <el-button type="primary" @click="onRegit('loginForm')">注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -25,30 +43,36 @@
         //全局值
         data(){
           return{
-            name:"",
+            loginForm: {
+              loginName: '',
+              loginPassword: ''
+            },
+            // 表单验证，需要在 el-form-item 元素中增加 prop 属性
+            loginFormRules: {
+              loginName: [
+                {required: true, message: '账号不可为空', trigger: 'blur'}
+              ],
+              loginPassword: [
+                {required: true, message: '密码不可为空', trigger: 'blur'}
+              ]
+            }
           }
         },
         //普通方法
         methods:{
           //登录方法
-          login(){
-            //严格要求接口方法体只能写传入值的别名，不得通过this等方法指向值
-            var userName = "123";
-            var userCode = "1";
-            var user = {
-              name:userName,
-              password:userCode
-            };
-
-            if(!isDataNull(userName) && !isDataNull(userCode)){
-              loginParameter.loginParameter(userName,userCode).then(data => {
-                console.log(data);
-              }).catch(e => {
-                console.log(e)
-              })
-            }
-
-          },
+          loginSubmit(formName) {
+            // 为表单绑定验证功能
+            this.$refs[formName].validate((valid) => {
+              if (valid) {
+                // 使用 vue-router 路由到指定页面，该方式称之为编程式导航
+                console.log("to loginSuccess");
+                this.$router.replace("/loginSuccess");
+              } else {
+                return false;
+              }
+            });
+          }
         },
         //监听
         watch:{
@@ -62,6 +86,6 @@
 </script>
 
 <style lang="scss" scoped>
-  @import "../../common/util/common.css";
+  @import "../../common/util/common";
 
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 Vue.config.productionTip = false;
 
 import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.use(Element);
 
