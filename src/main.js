610dc6ef498dc587d97a642a5106aa4e4113564f
WangMingZhi123 <Wmz19960224@163.com>
2019-12-27 13:51:42 +0800

    降低webpack版本

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from './router'
 Vue.config.productionTip = false;
 
 import Element from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Element);
 
