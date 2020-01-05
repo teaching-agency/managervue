32a1b9e084e0ed891c462751037f849a76685822
WangMingZhi123 <Wmz19960224@163.com>
2020-01-05 17:14:19 +0800

    创建菜单

--- src/components/page/model/menupage.vue
+++ src/components/page/model/menupage.vue
@@ -2,9 +2,54 @@
   <div class="container">
     <!--布局②-->
     <el-container style="height: 100%" direction="vertical">
-      <el-header>Header{{firstMenus}}</el-header>
+      <el-header>
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-menu-item index="1">客户管理</el-menu-item>
+          <el-menu-item index="2">资源维护</el-menu-item>
+          <el-menu-item index="3">下载中心</el-menu-item>
+          <el-menu-item index="4">个人中心</el-menu-item>
+          <el-menu-item index="5">系统设置</el-menu-item>
+        </el-menu>
+      </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="202px">
+              <el-menu
+                :default-active="index"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+                <el-submenu index="1" v-if="this.index.substring(0,1) == 1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>客户管理</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1">客户列表</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+                <el-submenu index="2" v-if="this.index.substring(0,1) == 2">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>下载中心</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="2-1">标签查询</el-menu-item>
+                    <el-menu-item index="2-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+              </el-menu>
+        </el-aside>
         <el-container>
           <el-main>Main</el-main>
           <el-footer>Footer</el-footer>
@@ -24,7 +69,8 @@
       mounted() {},
       data(){
         return{
-
+          activeIndex2:"1",
+          index:"1-1"
         }
       },
       computed:{
@@ -51,6 +97,16 @@
         }),
     },
     methods:{
+      handleSelect(key, keyPath) {
+        this.index = key.toString() + "-" + "1";
+        console.log(key, keyPath,this.index);
+      },
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
 
     },
     watch:{
